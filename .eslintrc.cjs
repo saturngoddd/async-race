08b720c9e3f410c60e98f789db81615bd09a09fa
saturngoddd <lilsatoru@gmail.com>
2024-04-12 21:08:43 +0600

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,24 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'max-lines-per-function': ['error', 40],
+    'no-magic-numbers': ['error', { ignoreArrayIndexes: true }],
   },
 };
